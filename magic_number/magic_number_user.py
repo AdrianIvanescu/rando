e1e7782ff5cefe32fa86cce2e57c95a566a571d4
Adrian Ivanescu <adrian.ivanescu@outlook.com>
2021-12-09 02:19:19 +0100

    reset_random_number

--- magic_number/magic_number_user.py
+++ magic_number/magic_number_user.py
@@ -1,18 +1,11 @@
 # 
 # guess a number between 1 and 100 << user input version >>
 # 
-import random
+from .utils import generate_random, print_win
 from colorama.ansi import Fore, Style
 
-def generate_random(min, max) -> int:
-    return random.randint(min,max)
-
-def print_win(number):
-    print(f">>You win! The magic number is: {Fore.GREEN}{number}{Style.RESET_ALL}")    
-
-number = generate_random(1,100)
-
 def guess_number_user():
+    number = generate_random(1,100)
     for answer in range(1,100):
         answer = int(input('Guess a number between 1-100: '))
         if answer > number:
@@ -21,5 +14,6 @@ def guess_number_user():
             print("Try again. Go higher...")
         if answer == number:
             print_win(answer)
+            number = generate_random(1,100)
             break
 
