8f688a1efc28fd6d8364ed907831410d96aacf9d
AdrianIvanescu <91798167+AdrianIvanescu@users.noreply.github.com>
2021-12-12 14:20:28 +0100

    create_db (#5)

---
+++ db_layer/create_table.py
@@ -0,0 +1,54 @@
+import sqlite3
+from sqlite3.dbapi2 import SQLITE_FUNCTION, Error
+
+from colorama.ansi import Fore, Style
+
+class MagicTable:
+    def __init__(self,db_name,table_name):
+        self.db_name = db_name
+        self.table_name = table_name
+    
+    def open_db_connection(self):
+        # connecting to sqlite
+        self.conn = sqlite3.connect(f"{self.db_name}.db")
+        
+        # creating a cursor object using the cursor() method
+        self.cursor = self.conn.cursor()
+
+    def commit_db_change(self):
+        # commit your changes in the database
+        self.conn.commit()
+
+    def close_db_connection(self):
+        # closing the connection
+        self.conn.close()
+
+    def execute_sql(self,sql):
+        try:
+            self.cursor.execute(sql)
+        except Exception as e:
+            print ('An exception occured', e)
+
+    def create_table(self):
+        # create table as per requirement
+        sql = f"""
+        CREATE TABLE IF NOT EXISTS {self.table_name}(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        magic_number INT(3) NOT NULL,
+        initial_guess INT(30) NOT NULL,
+        source CHAR(10) NOT NULL,
+        number_of_tries INT(2) NOT NULL
+        )
+        """
+        self.execute_sql(sql)
+        print(
+            f">> The table {Fore.GREEN}{self.table_name}{Style.RESET_ALL} created successfully..."
+        )
+
+    def drop_table(self):
+        # drop table if already exists.
+        sql = f"DROP TABLE IF EXISTS {self.table_name}"
+        self.execute_sql(sql)
+        print(
+            f">> The table {Fore.GREEN}{self.table_name}{Style.RESET_ALL} was drop successfully..."
+        )
