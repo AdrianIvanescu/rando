461d7343991f2c4d5804ade4ae4481ca80a2fe91
AdrianIvanescu <91798167+AdrianIvanescu@users.noreply.github.com>
2021-12-15 21:35:31 +0100

    adding a db dump command (#13)
    additional refactoring
    closes #12

--- db_layer/db_connection.py
+++ db_layer/db_connection.py
@@ -3,11 +3,15 @@ import sqlite3
 
 class DbConnection:
     def __init__(self, db_name):
-        self.db_name = db_name
+        self._db_name = db_name
+
+    @property
+    def db_name(self):
+        return self._db_name
 
     def open_db_connection(self):
         # connecting to sqlite
-        self.conn = sqlite3.connect(f"{self.db_name}.db")
+        self.conn = sqlite3.connect(f"{self._db_name}.db")
 
         # creating a cursor object using the cursor() method
         self.cursor = self.conn.cursor()
@@ -15,3 +19,13 @@ class DbConnection:
     def close_db_connection(self):
         # closing the connection
         self.conn.close()
+
+    def execute_sql(self, sql):
+        try:
+            self.cursor.execute(sql)
+        except Exception as e:
+            print('An exception occured >>', e)
+
+    def commit_db_change(self):
+        # commit your changes in the database
+        self.conn.commit()
--- db_layer/insert_field.py
+++ db_layer/insert_field.py
@@ -1,12 +1,12 @@
 from datetime import datetime
 
-from db_layer.create_table import MagicTable
+from db_layer.db_connection import DbConnection
 
 
-class InsertField(MagicTable):
+class InsertField():
     def __init__(self, db_name, table_name):
-        self.db_name = db_name
         self.table_name = table_name
+        self.db_conn = DbConnection(db_name)
 
     def insert_field(self, source, magic_number, number_of_tries, created_on):
         sql = f"""
@@ -14,13 +14,13 @@ class InsertField(MagicTable):
         number_of_tries,created_on)
         VALUES ('{source}','{magic_number}','{number_of_tries}','{created_on}')
         """
-        self.execute_sql(sql)
+        self.db_conn.execute_sql(sql)
 
 
 def save_to_db(number, num_of_tries, source):
     created_on = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     save_info = InsertField('magic_number', 'run_times')
-    save_info.open_db_connection()
+    save_info.db_conn.open_db_connection()
     save_info.insert_field(source, number, num_of_tries, created_on)
-    save_info.commit_db_change()
-    save_info.close_db_connection()
+    save_info.db_conn.commit_db_change()
+    save_info.db_conn.close_db_connection()
