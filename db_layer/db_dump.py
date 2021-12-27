646e7b1627ad59806f1a3890c7460f80113cd9d4
git <adrian.ivanescu@gmail.com>
2021-12-27 13:36:48 +0200

    fix tox -e lint for db_layer/db_dump.py

--- db_layer/db_dump.py
+++ db_layer/db_dump.py
@@ -14,8 +14,11 @@ class DbDump():
         path = './magic_number_dumps/'
         os.mkdir(path) if not os.path.isdir(path) else None
 
-        subprocess.call(["sqlite3", f"{self.db_conn.db_name}.db",
-        f".output {path}{self.db_conn.db_name}_{created_on}.sql", ".dump"])
+        subprocess.call([
+            "sqlite3",
+            f"{self.db_conn.db_name}.db",
+            f".output {path}{self.db_conn.db_name}_{created_on}.sql",
+            ".dump"])
 
     def dump_execute(self) -> None:
         self.db_conn.open_db_connection()
